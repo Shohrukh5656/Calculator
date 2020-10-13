415674e67104334c3f045e98948f54602ddfe672
SHOHRUKH <max.5656@bk.ru>
2020-10-13 20:04:06 +0500

    first commit

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ function clean() {
     document.form.textview.value = "";
 }
 function back() {
-    var exp = document.form.textview.value;
+    const exp = document.form.textview.value;
     document.form.textview.value = exp.substring(0, exp.length-1);
 }
 function equal() {
-    var exp = document.form.textview.value;
+    const exp = document.form.textview.value;
     if (exp) {
         document.form.textview.value = eval(exp);
     }
